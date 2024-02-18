eb84b5becbffa91bea76fec600fcc71ca8d90f32
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2024-02-18 16:41:54 -0500

    UI update

--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -5,43 +5,45 @@ import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import * as CanvasJS from '../../../assets/canvasjs.min.js';
 
 @Component({
-    selector: 'app-canvas',
-    templateUrl: './canvas.component.html',
-    styleUrls: ['./canvas.component.scss']
+  selector: 'app-canvas',
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.scss']
 })
 export class CanvasComponent implements OnInit, AfterViewInit {
 
-    @Input() data: Feature;
+  @Input() data: Feature;
 
-    constructor() { }
+  constructor() { }
 
-    ngOnInit(): void {
-        console.log(this.data.attributes);
+  ngOnInit(): void {
+    console.log(this.data.attributes);
+  }
+
+  ngAfterViewInit(): void {
+    if (this.data.attributes.A_CASA || this.data.attributes.A_HOSPITAL || this.data.attributes.A_HOSPITAL_UCI) {
+      this.loadChart();
     }
+  }
 
-    ngAfterViewInit(): void {
-        this.loadChart();
-    }
-
-    loadChart(): void {
-        const chart = new CanvasJS.Chart(this.data.attributes.NOMBRE_MPIO, {
-            theme: 'dark2',
-            width: 450,
-            animationEnabled: true,
-            title: { text: this.data.attributes.NOMBRE_MPIO },
-            data: [
-                {
-                    type: 'pie',
-                    indexLabel: '{name} - {y}',
-                    showInLegend: true,
-                    dataPoints: [
-                        { y: this.data.attributes.A_CASA, name: 'En casa' },
-                        { y: this.data.attributes.A_HOSPITAL, name: 'En hospitales (No UCI)' },
-                        { y: this.data.attributes.A_HOSPITAL_UCI, name: 'En UCI' }
-                    ]
-                }
-            ]
-        });
-        chart.render();
-    }
+  loadChart(): void {
+    const chart = new CanvasJS.Chart(this.data.attributes.NOMBRE_MPIO, {
+      theme: 'dark2',
+      width: 450,
+      animationEnabled: true,
+      title: { text: this.data.attributes.NOMBRE_MPIO },
+      data: [
+        {
+          type: 'pie',
+          indexLabel: '{name} - {y}',
+          showInLegend: true,
+          dataPoints: [
+            { y: this.data.attributes.A_CASA, name: 'En casa' },
+            { y: this.data.attributes.A_HOSPITAL, name: 'En hospitales (No UCI)' },
+            { y: this.data.attributes.A_HOSPITAL_UCI, name: 'En UCI' }
+          ]
+        }
+      ]
+    });
+    chart.render();
+  }
 }
